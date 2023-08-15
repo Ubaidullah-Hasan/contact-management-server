86e8a3564a8b33b9500ed8ab6534c14c21f05859
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-08-15 20:00:26 +0600

    mongoDB connect

--- index.js
+++ index.js
@@ -1,6 +1,82 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express()
 const port = process.env.PORT || 4000;
+
+console.log(process.env.DB_password)
+
+
+
+
+
+
+
+
+
+
+
+//  MONGODB connection start ******************************
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.clipjzr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // contactManage
+        // contacts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+//  MONGODB connection end ******************************
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
