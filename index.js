2cb8367ee41669450fdd9c16b9880241f0b7c3fc
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-08-16 18:35:08 +0600

    update contact

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 var cors = require('cors')
+const jwt = require("jsonwebtoken");
+
 
 
 const app = express()
@@ -11,6 +13,24 @@ const port = process.env.PORT || 4000;
 
 
 
+
+
+// Jwt verigy function
+// const verifyJWT = (req, res, next) => {
+//     console.log("hitting JWT")
+//     const autorization = req.headers.autorization;
+//     if (!autorization) {
+//         return res.status(401).send({ error: true, message: "unautorized access" })
+//     }
+//     const token = autorization.split(" ")[1];
+//     jwt.verify(token, process.env.ACCESS_KEY_SECRET, (error, decoded) => {
+//         if (error) {
+//             return res.status(401).send({ error: true, message: "unautorized access" })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+// }
 
 
 
@@ -46,6 +66,17 @@ async function run() {
         const contactCollection = client.db("contactManage").collection("contacts");
 
 
+        // ACCESS KEY JWT START
+        // app.post("/jwt", (req, res) => {
+        //     const user = req.body;
+        //     console.log(user);
+        //     const token = jwt.sign(user, process.env.ACCESS_KEY_SECRET, { expiresIn: "1h" })
+        //     res.send({ token });
+        // })
+        // ACCESS KEY JWT END
+
+
+
         //  *************** contactCollection ***************
         app.post("/contacts", async(req, res)=>{
             const userInfo = req.body;
@@ -53,12 +84,42 @@ async function run() {
             const data = {
                 name: userInfo?.name,
                 email: userInfo?.email,
-                phone: userInfo?.number,
-                image: userInfo?.imgURL,
+                phone: userInfo?.phone,
+                image: userInfo?.image,
+                category: userInfo?.category,
             }
             const result = await contactCollection.insertOne(data);
             res.send(result);
         })
+
+        app.get("/contacts", async(req, res)=>{
+            const result = await contactCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.put("/contacts/:id", async (req, res) => {
+            const id = req.params.id;
+            const body = req.body;
+
+            try {
+                const filter = { _id: new ObjectId(id) };
+                const update = {
+                    $set: {
+                        name: body.name,
+                        email: body.email,
+                        phone: body.phone,
+                        category: body.category,
+                    }
+                };
+
+                const result = await contactCollection.updateOne(filter, update);
+                res.send(result);
+            } catch (error) {
+                console.error("Error updating contact:", error);
+                res.status(500).send("Error updating contact");
+            }
+        });
+
 
 
 
